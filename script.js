e3b27e0c0cb5ba5bc93d417c67cd675fff69dbb0
shpokas <s.shpokas@gmail.com>
2024-09-20 14:16:47 +0300

    Edited JS code to make it DRY

--- script.js
+++ script.js
@@ -10,11 +10,43 @@ const count = 5;
 let mode = "";
 let selectedClr = "";
 
-//Event lstn for mode
+//Function for a color DIV
+
+function createColorDiv(hexCode) {
+  const colorDiv = document.createElement("div");
+
+  colorDiv.style.backgroundColor = hexCode;
+  colorDiv.classList.add("clr");
+  colorDiv.style.position = "relative";
+  colorDiv.style.cursor = "pointer";
+  colorDiv.style.width = "100%";
+
+  const hexText = document.createElement("span");
+
+  hexText.textContent = hexCode;
+  hexText.style.position = "absolute";
+  hexText.style.bottom = "10px";
+  hexText.style.left = "10px";
+  hexText.style.color = "#fff";
+  colorDiv.appendChild(hexText);
+
+  // Feature -> Copy HEx code to clipboard
+  colorDiv.addEventListener("click", () => {
+    navigator.clipboard
+      .writeText(hexCode)
+      .then(() => alert(`Hex code ${hexCode} copied to clipboard!`))
+      .catch((err) => console.error(`Failed to copy: `, err));
+  });
+
+  return colorDiv;
+}
+
+//Event listener for mode variable
 modeSelect.addEventListener("change", function (e) {
   mode = e.target.value;
 });
-//Event lstn for selectedClr
+
+//Event listener for selectedClr variable
 seedClrPicker.addEventListener("input", function (e) {
   selectedClr = e.target.value;
 });
@@ -26,7 +58,7 @@ gnrtBtn.addEventListener("click", function () {
     ""
   )}&mode=${mode}&count=${count}`;
 
-  //fetch API color scheme
+  //Fetch API color scheme
   fetch(apiUrl)
     .then((res) => {
       //check if no errors
@@ -36,48 +68,12 @@ gnrtBtn.addEventListener("click", function () {
       return res.json();
     })
     .then((data) => {
-      //extract scheme data from api
-      const colors = data.colors;
-      // CLEAR
+      // Clear
+
       colorSchemeContainer.innerHTML = "";
-
-      // colorSchemeContainer.innerHTML = "";
-
-      colors.forEach((color) => {
+      data.colors.forEach((color) => {
         const hexCode = color.hex.value;
-
-        const colorDiv = document.createElement("div");
-
-        colorDiv.style.backgroundColor = hexCode;
-        colorDiv.style.backgroundColor = color.hex.value;
-        //Styling div
-        colorDiv.style.width = "100%";
-        colorDiv.classList.add("clr");
-        colorDiv.style.position = "relative";
-        colorDiv.style.cursor = "pointer";
-
-        // Add hex code inside div
-        const hexText = document.createElement("span");
-        hexText.textContent = hexCode;
-        hexText.style.position = "absolute";
-        hexText.style.bottom = "10px";
-        hexText.style.left = "10px";
-        hexText.style.color = "#fff";
-        colorDiv.appendChild(hexText);
-
-        // Feature - copy text to clipboard
-
-        colorDiv.addEventListener("click", function () {
-          navigator.clipboard
-            .writeText(hexCode)
-            .then(() => {
-              alert(`Hex code ${hexCode} copied to clipboard!`);
-            })
-            .catch((err) => {
-              console.error(`Failed to copy: `, err);
-            });
-        });
-
+        const colorDiv = createColorDiv(hexCode);
         colorSchemeContainer.appendChild(colorDiv);
       });
     })
